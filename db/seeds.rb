51f19ca6381e78c674bb9899cac530b2d62588c3
Chris Williams <chriswilliams14.williams@gmail.com>
2019-11-13 14:30:03 +0000

    Task 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Restaurant.destroy_all
+5.times do
+  Restaurant.create!({ name: Faker::Ancient.god, address: Faker::Address.city, phone_number: Faker::PhoneNumber.cell_phone, category: 'chinese' })
+end
+
